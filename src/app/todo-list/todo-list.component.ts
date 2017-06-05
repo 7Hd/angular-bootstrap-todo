f105c0c89c179df2ee0d882df655a869d7bf9c1e
7Hd <kevin1200x@gmail.com>
2017-06-05 22:36:33 +0800

    chore: add component todo-list.
    create by command: `ng generate component todo-list --inline-style`

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'td-todo-list',
+  templateUrl: './todo-list.component.html',
+  styles: []
+})
+export class TodoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
