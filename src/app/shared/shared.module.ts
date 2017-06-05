8d4effab0eb8c91ce9951682bdd2b5a620655dcf
7Hd <kevin1200x@gmail.com>
2017-06-05 22:43:07 +0800

    chore: add pipe enum-array.
    create by command: `ng generate pipe shared\enum-array --export shared`

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EnumArrayPipe } from './enum-array.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [EnumArrayPipe],
+  exports: [EnumArrayPipe]
 })
 export class SharedModule { }
